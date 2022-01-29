b6f05096fef7efe970e7b8329844e73d45ede229
aman-eth <aman.offficial@gmail.com>
2022-01-29 17:23:45 +0530

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xA1c7CFF33f3C298A0706a20d40b59b24be0B7e69";
+const METADATA_URL = "https://nft-collection-aman-eth.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  const metadataURL = METADATA_URL;
+
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract,
+  );
+
+  console.log(
+    "Crypto Devs Contract Address: ",
+    deployedCryptoDevsContract.address,
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
